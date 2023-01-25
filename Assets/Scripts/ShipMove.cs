27a00f1fb4f2d81fe7676695fa0401a0d422bfe8
Frostnox <51527640+Frostnox@users.noreply.github.com>
2023-01-25 00:54:08 -0800

    Test commit

--- Assets/Scripts/ShipMove.cs
+++ Assets/Scripts/ShipMove.cs
@@ -25,6 +25,7 @@ public class ShipMove : MonoBehaviour
         // Update Rotation
         transform.rotation = spawnRotation;
     }
+    
     void Update()
     {
         // Move in direction set by move direction
